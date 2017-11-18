98793dcd3997f21ce257786ec5f4de1e41ac1b09
Rabit <home@rabits.org>
2017-11-17 21:09:36 -0800

    Forget to add init to modules dir

---
+++ src/modules/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''Modules
+This package contains modules that could be loaded by Core according to the configuration & dependencies
+'''
+
+from . import *
